07f9e0e72e89d6ebebd42dc4296c6d1ca975553f
bonnie.hung <bonnie.hung@accenture.com>
2020-01-03 14:50:28 -0500

    added auth stack

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Platform } from 'react-native';
 
 import MainTabNavigator from './MainTabNavigator';
+import SignInScreen from '../screens/SignInScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const AuthStack = createStackNavigator(
+  {
+    SignIn: SignInScreen
+  },
+  config 
+);
 
 export default createAppContainer(
-  createSwitchNavigator({
+  createSwitchNavigator(
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
+    
+    {
+      Main: MainTabNavigator,
+    // Auth: AuthStack,
+    },
+  )
 );
