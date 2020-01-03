5d3e2509d41c51232dc29b31e0c585fe264787b2
bonnie.hung <bonnie.hung@accenture.com>
2020-01-03 14:03:37 -0500

    created LoggedOut component

---
+++ screens/LoggedOut.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    Scrollview,
+} from 'react-native';
+
+export default class LoggedOut extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Logged Out</Text>
+            </View>
+        );
+    }
+}
